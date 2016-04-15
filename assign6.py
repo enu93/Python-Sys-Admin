d673440af922b3bfedccd212f4d5bf3f4f1bca71
Inn Thapa <innuthapa93@gmail.com>
2016-04-15 00:15:18 -0500

    replace ; with , for input

--- assign6.py
+++ assign6.py
@@ -8,3 +8,8 @@ file = open(input_file)
 data = file.readlines()
 file.close()
 print data
+
+for d in data:
+	replace_comma = d.replace(";", ", ")
+	print replace_comma
+	# createUser(replace_comma)
