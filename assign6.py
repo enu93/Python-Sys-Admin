a42e56ac4890188b7b4d3e1998b4e4444cbe3685
Innu Thapa <innuthapa93@gmail.com>
2016-04-15 00:37:19 -0500

    convert input data and define create user function

--- assign6.py
+++ assign6.py
@@ -2,14 +2,22 @@
 import os
 import sys
 
+def createUser(firstName, lastName, password, uid, gid):
+	fname = firstName[:1].lower()
+	lname = lastName[:7].lower()
+	name = fname + lname
+
 # Read input from command line
 input_file = sys.argv[1]
 file = open(input_file)
-data = file.readlines()
+data = file.read().splitlines()
 file.close()
-print data
 
-for d in data:
-	replace_comma = d.replace(";", ", ")
-	print replace_comma
-	# createUser(replace_comma)
+for data in data:
+	input_data = data.split(";")
+	firstName = input_data[0]
+	lastName = input_data[1]
+	password = input_data[2]
+	uid = input_data[3]
+	gid = input_data[4]
+	createUser(firstName,lastName, password, uid, gid)
